3fa03668e66b89a60e2cb7d3ab33dc694da3a29f
山口玲偉 <rei@REInoMacBook-Air.local>
2022-01-16 11:31:23 +0900

    env修正

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,26 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+    if Rails.env.production? || Rails.env.development?
+        config.storage :fog
+        config.fog_provider = 'fog/aws'
+        config.fog_directory  = 's3rybucket'
+        config.asset_host = 'https://s3-us-east-2.amazonaws.com/s3rybucket'
+        config.asset_host = 'https://ry3test.s3.amazonaws.com'
+
+        config.fog_public = false
+
+
+        config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキー
+        aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
+        region: 'ap-northeast-1'
+        }
+
+
+        # config.cache_storage = :fog
+    end
+  end
