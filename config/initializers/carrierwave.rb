0980df9c979c5d8f7e8934bde6cfad42b97c75ca
山口玲偉 <rei@REInoMacBook-Air.local>
2022-01-16 13:39:14 +0900

    herokuデプロイ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,24 +3,22 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-    if Rails.env.production? || Rails.env.development?
-        config.storage :fog
-        config.fog_provider = 'fog/aws'
-        config.fog_directory  = 's3rybucket'
-        config.asset_host = 'https://s3-us-east-2.amazonaws.com/s3rybucket'
-        config.asset_host = 'https://ry.s3.amazonaws.com'
 
-        config.fog_public = false
-
-
-        config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキー
-        aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
-        region: 'ap-northeast-1'
-        }
-
-
-        # config.cache_storage = :fog
-    end
+  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 's3rybucket'
+    config.asset_host = 'https://s3-ap-northeast1.amazonaws.com/s3rybucket'
+    config.asset_host = 'https://s3rybucket.s3.amazonaws.com'
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET'],
+      region: 'ap-northeast-1',
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
+end
